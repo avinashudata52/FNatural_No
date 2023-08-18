bb0771ce3e89e8cbab6f83397d45ec9acb54d67a
Aviansh Udata <60933551+avinashudata52@users.noreply.github.com>
2023-08-18 09:31:12 +0530

    Add files via upload
    This is the Simple First Natural Number Program for begginers to understand how code is implemets.

---
+++ FNaturalNo.java
@@ -0,0 +1,15 @@
+public class FNaturalNo
+{
+	public static void main(String args[])
+	{
+		int  n=6;
+		int sum=0;
+		
+		for (int i=1; i<=n; i++)
+		
+		sum +=i;
+		
+		System.out.println(sum);
+		
+	}
+}
